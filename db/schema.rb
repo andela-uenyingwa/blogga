ea3458f917c69c57b39de4acf2b8d3bf23b6c8ec
andela-uenyingwa <uloaku.enyingwa@andela.com>
2017-07-18 22:38:48 +0100

    finish comments feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170702214610) do
-
+ActiveRecord::Schema.define(version: 20_170_708_170_744) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'articles', force: :cascade do |t|
+    t.string   'title'
+    t.text     'body'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
+  create_table 'comments', force: :cascade do |t|
+    t.string   'author_name'
+    t.text     'body'
+    t.integer  'article_id'
+    t.datetime 'created_at',  null: false
+    t.datetime 'updated_at',  null: false
+    t.index ['article_id'], name: 'index_comments_on_article_id', using: :btree
+  end
+
+  add_foreign_key 'comments', 'articles'
 end
